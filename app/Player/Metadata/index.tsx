01765589ed9f0b43e36bd86c7cc7069a5ce89ecf
Gabriel Maizo <90287831+Undead34@users.noreply.github.com>
2023-01-04 15:50:03 +0000

    Folder Update

--- app/Player/Metadata/index.tsx
+++ app/Player/Metadata/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css";
-import { Track } from "./types";
+import { Track } from "../types";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -12,17 +12,6 @@ export default function Metadata(props: Props) {
     <div className="flex-1 px-[72px]">
       <div className="m-auto w-4/5 max-w-[800px]">
         <div className="flex items-center">
-          <div>
-            <div className="mr-1 h-[28px] w-[28px]">
-              <Image
-                className="rounded-sm"
-                width={260}
-                height={260}
-                src={props.track.artwork.src}
-                alt="Artwork"
-              />
-            </div>
-          </div>
           <div className="felx-1 mr-1 overflow-hidden">
             <div className="relative overflow-hidden whitespace-nowrap">
               <span
@@ -39,11 +28,32 @@ export default function Metadata(props: Props) {
             </div>
           </div>
           <div className="flex w-full">
-            <button>K</button>
-            <button>L</button>
-            <button>V</button>
-            <button>N</button>
-            <button>+</button>
+            <button>
+              <svg
+                className="h-4 w-4 fill-current text-white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15 4.5a3.5 3.5 0 1 0-7 0 3.5 3.5 0 0 0 7 0zm1 0a4.5 4.5 0 0 1-5.099 4.46L3.048 15 0 12l7-7.58v.08a4.5 4.5 0 1 1 9 0zM7.166 5.715l-5.774 6.252 1.736 1.71 6.57-5.053a4.511 4.511 0 0 1-2.532-2.91z"></path>
+              </svg>
+            </button>
+            <button>
+              <svg
+                viewBox="0 0 16 16"
+                focusable="false"
+                className="h-4 w-4 fill-current"
+              >
+                <path d="M8 3.266C2.837-2.68-2.564 4.578 1.328 8.516 5.22 12.451 8 15 8 15s2.78-2.548 6.672-6.485C18.564 4.501 13.162-2.679 8 3.265z"></path>
+              </svg>
+            </button>
+            <button>
+              <svg
+                viewBox="0 0 16 16"
+                focusable="false"
+                className="h-4 w-4 fill-current "
+              >
+                <path d="M9 0H8v7H1v1h7v7h1V8h7V7H9V0z"></path>
+              </svg>
+            </button>
           </div>
         </div>
         <div>
